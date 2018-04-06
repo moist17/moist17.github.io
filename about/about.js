ffae5a2467fa59f46837b6c73e6b71d9c6493122
rottenmushroom <aphasia@Lizs-MacBook-Pro.local>
2018-04-06 11:17:07 +0100

    About this project

---
+++ about/about.js
@@ -0,0 +1,25 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+
+$(window).on('scroll', function(){
+    if ($(window).scrollTop()){
+        console.log('testing');
+        $('nav').addClass('resizing');
+    }
+    else {
+        $('nav').removeClass('resizing');
+    }
+})
