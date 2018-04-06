7365d53c5b365572d6b1ab24b6d9e718db2c029c
rottenmushroom <aphasia@Lizs-MacBook-Pro.local>
2018-04-06 16:51:27 +0100

    Product

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ Highcharts.chart('container', {
     }]
 });
 
-var availableDates = ["04/01/2018", "04/02/2018", "04/03/2018"];
+var availableDates = ["04/01/2018", "04/02/2018", "04/03/2018", "04/04/2018", "04/05/2018", "04/06/2018", "04/07/2018"];
 function available(date) {
     var string = jQuery.datepicker.formatDate('mm/dd/yy', date);
     return [ availableDates.indexOf(string) != -1];
@@ -81,13 +81,49 @@ $(function() {
             console.log("Selected date: " + dateText);
             if (dateText == '04/01/2018') {
                 $('.item').removeClass('show');
-                let day1 = ['#spaghetti', '#scarf', '#socks'];
+                let day1 = ['#p01', '#p03', '#p07'];
                  jQuery.each( day1, function( i, val ) {
                     $( val ).addClass('show');
                 });
             } else if (dateText == '04/02/2018') {
                 $('.item').removeClass('show');
-                let day2 = ['#mushroom', '#cn_leaf', '#o_juice'];
+                let day2 = ['#p02', '#p04', '#p05', '#p06', '#p08', '#p09', '#p31'];
+                jQuery.each( day2, function( i, val ) {
+                    $( val ).addClass('show');
+                });
+            } else if (dateText == '04/03/2018') {
+                $('.item').removeClass('show');
+                let day2 = ['#p30'];
+                jQuery.each( day2, function( i, val ) {
+                    $( val ).addClass('show');
+                });
+            } else if (dateText == '04/04/2018') {
+                $('.item').removeClass('show');
+                let day2 = ['#p10', '#p11', '#p12', '#p13', '#p14', '#p15'];
+                jQuery.each( day2, function( i, val ) {
+                    $( val ).addClass('show');
+                });
+            } else if (dateText == '04/05/2018') {
+                $('.item').removeClass('show');
+                let day2 = ['#p16', '#p17', '#p18'];
+                jQuery.each( day2, function( i, val ) {
+                    $( val ).addClass('show');
+                });
+            } else if (dateText == '04/06/2018') {
+                $('.item').removeClass('show');
+                let day2 = ['#p19', '#p20', '#p21', '#p22', '#p23', '#p24'];
+                jQuery.each( day2, function( i, val ) {
+                    $( val ).addClass('show');
+                });
+            } else if (dateText == '04/06/2018') {
+                $('.item').removeClass('show');
+                let day2 = ['#p25', '#p26', '#p27'];
+                jQuery.each( day2, function( i, val ) {
+                    $( val ).addClass('show');
+                });
+            } else if (dateText == '04/07/2018') {
+                $('.item').removeClass('show');
+                let day2 = ['#p29'];
                 jQuery.each( day2, function( i, val ) {
                     $( val ).addClass('show');
                 });
@@ -100,7 +136,7 @@ $(function() {
     $( "#food" ).click(function() {
         $('.item').removeClass('show');
         event.preventDefault();
-        let food = ['#fox_cookie', '#pad_thai', '#o_juice', '#spaghetti', '#ice_cream', 'cn_leaf', 'mushroom', 'chicken_burger', 'cheese_powder', 'chocolate_milk'];
+        let food = ['#p01', '#p02', '#p03', '#p04', '#p08', '#p09', '#p10', '#p11', '#p12', '#p13', '#p17', '#p19', '#p20', '#p21', '#p22', '#p23', '#p24', '#p25', '#p26', '#p27'];
         jQuery.each( food, function( i, val ) {
             $( val ).addClass("show");
         });
@@ -108,7 +144,7 @@ $(function() {
     $( "#shopping" ).click(function() {
         $('.item').removeClass('show');
         event.preventDefault();
-        let shopping = ['#shopping', '#socks', '#red_scarf'];
+        let shopping = ['#p05', '#p06', '#p16'];
         jQuery.each( shopping, function( i, val ) {
             $( val ).addClass("show");
         });
@@ -116,7 +152,23 @@ $(function() {
     $( "#supplies" ).click(function() {
         $('.item').removeClass('show');
         event.preventDefault();
-        let supplies = ['#bowl', '#shower_gel', '#tissue_paper'];
+        let supplies = ['#p7', '#p14', '#p15'];
+        jQuery.each( supplies, function( i, val ) {
+            $( val ).addClass("show");
+        });
+    })
+    $( "#social" ).click(function() {
+        $('.item').removeClass('show');
+        event.preventDefault();
+        let supplies = ['#p18', '#p29', '#p30', '#p31'];
+        jQuery.each( supplies, function( i, val ) {
+            $( val ).addClass("show");
+        });
+    })
+    $( "#medical" ).click(function() {
+        $('.item').removeClass('show');
+        event.preventDefault();
+        let supplies = ['#p28'];
         jQuery.each( supplies, function( i, val ) {
             $( val ).addClass("show");
         });
